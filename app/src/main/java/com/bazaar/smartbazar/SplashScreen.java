0b17a678bb481fe879170ecf0c68a6d38110d961
Abhi <avvjit@gmail.com>
2023-08-27 16:35:13 +0530

    Initial commit

---
+++ app/src/main/java/com/bazaar/smartbazar/SplashScreen.java
@@ -0,0 +1,19 @@
+package com.bazaar.smartbazar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
